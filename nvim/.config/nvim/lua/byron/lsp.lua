16f755fb02fac0f9af9f75d8ee164927b9f78ea2
Byron <37745048+byhowe@users.noreply.github.com>
2022-11-10 20:37:02 +0300

    [nvim] temporarily fix annoying prompt

--- nvim/.config/nvim/lua/byron/lsp.lua
+++ nvim/.config/nvim/lua/byron/lsp.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.setup = function()
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
 
   local on_attach = function(client, bufnr)
