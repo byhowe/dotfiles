95be97a9e3e08fb803c00d4cce90053d7dae8b2e
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2023-04-02 18:31:54 +0300

    [doom git] Include full name and add contact email

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Ömer F. Çavuş"
-      user-mail-address "37745048+byhowe@users.noreply.github.com")
+(setq user-full-name "Ömer Faruk Çavuş"
+      user-mail-address "byhowe@proton.me")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
